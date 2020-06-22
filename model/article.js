c5d5ab90fc0c70c4f65fd372eb29b0eab82178b2
Shveta Kumar <sk@shveta.ca>
2020-06-22 11:07:03 -0400

    Created databases to store the articles and notes

---
+++ model/article.js
@@ -0,0 +1,26 @@
+//create the database to store the articles
+
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticlesSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    }, 
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
+});
+
+//Creates model based on the above schema using mongoose's models
+
+var Article = mongoose.model("Article", ArticlesSchema);
+
+module.exports = Article;
---
+++ model/notes.js
@@ -0,0 +1,14 @@
+//create the database to store the notes
+
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var NoteSchema = new Schema ({
+    title: String, 
+    body: String
+});
+
+var Note = mongoose.model("Note", NoteSchema);
+
+module.exports = Note;
