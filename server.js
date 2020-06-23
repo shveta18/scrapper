4b2decf20fe7eeddc3f8eb9af3db9be116e0369e
Shveta Kumar <sk@shveta.ca>
2020-06-22 22:44:04 -0400

    Changed port to 8080

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var cheerio = require("cheerio");
 //Require all models from the models folder
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = 8080;
 var app = express();
 
 // Use morgan logger for logging requests
@@ -63,7 +63,24 @@ app.get("/scrape", function (req, res) {
     });
 
 });
+// when user clicks the submit-note button, the note is attached to the article.
 
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("note")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 
 // Start the server
 app.listen(PORT, function () {
